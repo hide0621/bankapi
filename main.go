4dff4c4b055ace8b0bae1c62f24199dee10e94cb
hide0621 <hi19de72h2so4@gmail.com>
2022-06-17 13:49:54 +0900

    「銀行APIを作成する」のうち、「預金メソッドを公開する」まで

--- main.go
+++ main.go
@@ -23,12 +23,17 @@ func main() {
 
 	//「localhost:8000/statement」でハンドラ関数を実行
 	http.HandleFunc("/statement", statement)
+	//「localhost:8000/deposit」でハンドラ関数を実行
+	http.HandleFunc("/deposit", deposit)
 	//上記のハンドラ関数が実行されているエンドポイントでサーバーを起動
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
 
 //ハンドラ関数の定義
+//HTTPの呼び出しを通じて口座番号を送信する関数
+//例：http://localhost:8000/statement?number=1001
 func statement(w http.ResponseWriter, req *http.Request) {
+
 	//リクエストのクエリパラメータに書かれた口座番号を左辺の変数（文字列型）に代入
 	numberqs := req.URL.Query().Get("number")
 
@@ -45,6 +50,40 @@ func statement(w http.ResponseWriter, req *http.Request) {
 			fmt.Fprintf(w, "Account with number %v can't be found!", number)
 		} else {
 			fmt.Fprintf(w, "%v", account.Statement())
+		}
+	}
+}
+
+//ハンドラ関数の定義
+//預金に関する関数
+//例：http://localhost:8000/deposit?number=1001&amount=100
+func deposit(w http.ResponseWriter, req *http.Request) {
+
+	//リクエストのクエリパラメータに書かれた口座番号を左辺の変数（文字列型）に代入
+	numberqs := req.URL.Query().Get("number")
+	//リクエストのクエリパラメータに書かれた預金する金額を左辺の変数（文字列型）に代入
+	amountqs := req.URL.Query().Get("amount")
+
+	if numberqs == "" {
+		fmt.Fprintf(w, "Account number is missing!")
+		return
+	}
+
+	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid account number!")
+	} else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
+		fmt.Fprintf(w, "Invalid amount number!")
+	} else {
+		account, ok := accounts[number]
+		if !ok {
+			fmt.Fprintf(w, "Account with number %v can't be found!", number)
+		} else {
+			err := account.Deposit(amount)
+			if err != nil {
+				fmt.Fprintf(w, "%v", err)
+			} else {
+				fmt.Fprintf(w, "%v", account.Statement())
+			}
 		}
 	}
 }
