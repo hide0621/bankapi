53673890c4b03be0faa6d2f635ecd50967f44ce3
hide0621 <hi19de72h2so4@gmail.com>
2022-07-18 10:48:11 +0900

    黄色波線部分の修正（183行目のfmt.Fprintf部分）

--- main.go
+++ main.go
@@ -9,8 +9,8 @@ import (
 	"strconv"
 )
 
-//DBの代わりにmapを用いて口座情報を持つ
-var accounts = map[float64]*bank.Account{}
+//DBの代わりにmapを用いて口座情報、送金先情報を持つ
+var accounts = map[float64]*CustomAccount{}
 
 // CustomAccount ...
 type CustomAccount struct {
@@ -18,13 +18,26 @@ type CustomAccount struct {
 }
 
 func main() {
-	accounts[1001] = &bank.Account{
-		Customer: bank.Customer{
-			Name:    "John",
-			Address: "Los Angeles, California",
-			Phone:   "(213) 555 0147",
+	accounts[1001] = &CustomAccount{
+		Account: &bank.Account{
+			Customer: bank.Customer{
+				Name:    "John",
+				Address: "Los Angeles, California",
+				Phone:   "(213) 555 0147",
+			},
+			Number: 1001,
 		},
-		Number: 1001,
+	}
+
+	accounts[1002] = &CustomAccount{
+		Account: &bank.Account{
+			Customer: bank.Customer{
+				Name:    "Mark",
+				Address: "Irvine, California",
+				Phone:   "(949) 555 0198",
+			},
+			Number: 1002,
+		},
 	}
 
 	//「localhost:8000/statement」でハンドラ関数を実行
@@ -33,6 +46,8 @@ func main() {
 	http.HandleFunc("/deposit", deposit)
 	//「localhost:8000/withdraw」でハンドラ関数を実行
 	http.HandleFunc("/withdraw", withdraw)
+	//「localhost:8000/transfer」でハンドラ関数を実行
+	http.HandleFunc("/transfer", transfer)
 	//上記のハンドラ関数が実行されているエンドポイントでサーバーを起動
 	log.Fatal(http.ListenAndServe("localhost:8000", nil))
 }
@@ -106,10 +121,12 @@ func withdraw(w http.ResponseWriter, req *http.Request) {
 	//リクエストのクエリパラメータに書かれた預金する金額を左辺の変数（文字列型）に代入
 	amountqs := req.URL.Query().Get("amount")
 
+	//口座番号が空欄の時
 	if numberqs == "" {
 		fmt.Fprintf(w, "%v", "Account number is missing!")
 	}
 
+	//文字列型の各変数をfloat64型にして返す
 	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
 		fmt.Fprintf(w, "%v", "Invalid account number!")
 	} else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
@@ -129,16 +146,24 @@ func withdraw(w http.ResponseWriter, req *http.Request) {
 	}
 }
 
+//ハンドラ関数の定義
+//送金に関する機能
 func transfer(w http.ResponseWriter, req *http.Request) {
+
+	//リクエストのクエリパラメータに書かれた口座番号(送金元)を左辺の変数（文字列型）に代入
 	numberqs := req.URL.Query().Get("number")
+	//リクエストのクエリパラメータに書かれた送金する金額を左辺の変数（文字列型）に代入
 	amountqs := req.URL.Query().Get("amount")
+	//リクエストのクエリパラメータに書かれた送金先を左辺の変数（文字列型）に代入
 	destqs := req.URL.Query().Get("dest")
 
+	//口座番号が空欄の時
 	if numberqs == "" {
 		fmt.Fprintf(w, "Account number is missing!")
 		return
 	}
 
+	//文字列型の各変数をfloat64型にして返す
 	if number, err := strconv.ParseFloat(numberqs, 64); err != nil {
 		fmt.Fprintf(w, "Invalid account number!")
 	} else if amount, err := strconv.ParseFloat(amountqs, 64); err != nil {
@@ -155,7 +180,7 @@ func transfer(w http.ResponseWriter, req *http.Request) {
 			if err != nil {
 				fmt.Fprintf(w, "%v", err)
 			} else {
-				fmt.Fprintf(w, accountA.Statement())
+				fmt.Fprintf(w, "%v", accountA.Statement())
 			}
 		}
 	}
